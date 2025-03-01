5b2930351601cbb4eade9131094352f25f775e55
Rishabh Shetty <rishabhshettyvm@gmail.com>
2025-03-01 20:45:20 +0530

    dealing with form encoded data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, Request,  HTTPException
 import json
 import httpx
 
@@ -6,12 +6,16 @@ app = FastAPI()
 
 
 @app.post("/slack/interactivity")
-async def slack_interactivity(request, payload: str= Form(...)):
+async def slack_interactivity(request: Request, payload: str= Form(...)):
     try:
         action_payload = json.loads(payload)
 
         button_value = action_payload["actions"][0]["value"]
         print(f"button_value: {button_value}")
-    except:
-        print("in except")
-    return {"message": "Hello World"}
+        # async with httpx.AsyncClient() as client:
+        #     await client.post(MAKE_HTTP_REQUEST_URL, json={"button_value": button_value})
+
+        return {"text": "Response recorded"}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
