9531963621ff61899aeab3a49868adb1e309512f
Rishabh Shetty <rishabhshettyvm@gmail.com>
2025-03-01 20:50:49 +0530

    get value

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import json
 import httpx
 
 app = FastAPI()
-
+button = None
 
 @app.post("/slack/interactivity")
 async def slack_interactivity(request: Request, payload: str= Form(...)):
+    global button
     try:
         action_payload = json.loads(payload)
 
         button_value = action_payload["actions"][0]["value"]
         print(f"button_value: {button_value}")
+        button = button_value
         # async with httpx.AsyncClient() as client:
         #     await client.post(MAKE_HTTP_REQUEST_URL, json={"button_value": button_value})
 
@@ -19,3 +21,7 @@ async def slack_interactivity(request: Request, payload: str= Form(...)):
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+    
+@app.get("/get_value/")
+async def get_value():
+    return {"button_value": button}
